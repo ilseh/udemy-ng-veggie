3bbc0aef378b5359daeb4873dc0d25825fa0325d
Unknown <ilse.haanstra@yarden.nl>
2018-02-22 23:40:21 +0100

    Refactoring shopping cart and ordering

--- src/app/shopping/components/check-out/check-out.component.ts
+++ src/app/shopping/components/check-out/check-out.component.ts
@@ -4,6 +4,8 @@ import { ShoppingCartService } from "../../../shopping-cart.service";
 import { Subscription } from "rxjs/Subscription";
 import { OrderService } from "../../../order.service";
 import { AuthService } from "../../../auth.service";
+import { Order } from "../../../models/order";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-check-out',
@@ -20,12 +22,13 @@ export class CheckOutComponent implements OnInit, OnDestroy {
   constructor(
     private shoppingCartService: ShoppingCartService,
     private orderService: OrderService,
-    private authService: AuthService) {}
+    private authService: AuthService,
+    private router: Router) {}
 
   async ngOnInit() {
     const cart$ = await this.shoppingCartService.getCart();
     this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
-    this.authService.user$.subscribe(user => this.userId = user.uid);
+    this.userSubscription = this.authService.user$.subscribe(user => this.userId = user.uid);
   }
 
   ngOnDestroy() {
@@ -33,23 +36,11 @@ export class CheckOutComponent implements OnInit, OnDestroy {
     this.userSubscription.unsubscribe();
   }
 
-  placeOrder() {
-    const order = {
-      userId: this.userId,
-      datePlaced: new Date().getTime(),
-      shipping: this.shipping,
-      items: this.cart.items.map(i => {
-        return {
-            product: {
-              title: i.title,
-              imageUrl: i.imageUrl,
-              price: i.price
-            },
-            quantity: i.quantity,
-            totalPrice: i.totalPrice
-        }
-      })
-    }
-    this.orderService.storeOrder(order);
+  async placeOrder() {
+    const order = new Order(this.userId, this.shipping, this.cart);
+    const result = await this.orderService.placeOrder(order);
+
+    this.router.navigate((['/order-success', result.key]));
+
   }
 }
--- src/app/shopping/shopping.module.ts
+++ src/app/shopping/shopping.module.ts
@@ -22,7 +22,7 @@ import { FormsModule } from "@angular/forms";
       { path: 'shopping-cart', component: ShoppingCardComponent },
       { path: 'products', component: ProductsComponent },
       { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuardService] },
-      { path: 'order-success', component: OrderSuccessComponent, canActivate: [AuthGuardService] },
+      { path: 'order-success/:id', component: OrderSuccessComponent, canActivate: [AuthGuardService] },
       { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuardService] }
     ])
   ],
